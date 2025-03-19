9a91b84dbf28482d82111e313194991f669024d1
rohsuujin <rohsuujin@gmail.com>
2025-03-19 13:34:54 -0500

    Added Housig Interface and created a single interface of the new interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,15 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://www.google.com/maps/search/?api=1&query=';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: '${this.baseUrl}/example-house.jpg',
+    availabeUnits: 99,
+    wifi: true,
+    laundry: false
+  };
 }
